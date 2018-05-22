2339be04e48618a5acbe1f37ef72074ee70fa890
Dazmi <36841434+Dazmi@users.noreply.github.com>
2018-05-22 22:12:55 +1000

    Fixed Javascript

--- siteIndex/public_html/js/js.js
+++ siteIndex/public_html/js/js.js
@@ -17,102 +17,102 @@ function validateForm() {
 	// First Name
     var x = document.forms["regform"]["firstname"].value;
     if (x == "") {
-        alert("Name must be filled out");
+        alert("First name must be filled out");
         return false;
     }
 	if (x == "\W") {
-        alert("Name must contain characters");
+        alert("First name must contain characters");
         return false;
     }
 	if (x != "\D") {
-        alert("Must not contain numbers");
+        alert("First name must not contain numbers");
         return false;
 	}
 	if (x == "\s") {
-        alert("Must have no whitespace");
+        alert("First name must have no whitespace");
         return false;
 	}
 	
 	// Last Name
 	var x = document.forms["regform"]["lastname"].value;
     if (x == "") {
-        alert("Name must be filled out");
+        alert("Last name must be filled out");
         return false;
     }
 	if (x == "\W") {
-        alert("Name must contain characters");
+        alert("Last name must contain characters");
         return false;
     }
 	if (x != "\D") {
-        alert("Must not contain numbers");
+        alert("Last name must not contain numbers");
         return false;
 	}
 	if (x == "\s") {
-        alert("Must have no whitespace");
+        alert("Last name must have no whitespace");
         return false;
 	}
 	
 	// Username
 	var x = document.forms["regform"]["username"].value;
     if (x == "") {
-        alert("Name must be filled out");
+        alert("Username must be filled out");
         return false;
     }
 	if (x == "\W") {
-        alert("Name must contain alphanumeric characters");
+        alert("Username must contain alphanumeric characters");
         return false;
     }
 	if (x == "\s") {
-        alert("Must have no whitespace");
+        alert("Username must have no whitespace");
         return false;
     }
 		if (x == "^.{1,3}$") {
-        alert("Must contain at least 3 characters");
+        alert("Username must contain at least 3 characters");
         return false;
     }
 	
 	// Password
 	var x = document.forms["regform"]["password"].value;
     if (x == "") {
-        alert("Name must be filled out");
+        alert("Password must be filled out");
         return false;
     }
 	if (x == "\s") {
-        alert("Must have no whitespace");
+        alert("Password must have no whitespace");
         return false;
     }
-	if (x != "\d") {
-        alert("Must contain at least 1 digit character");
+	if (x == "\d") {
+        alert("Password must contain at least 1 digit character");
         return false;
     }
 	if (x != "[A-Z]") {
-        alert("Must contain at a capital character");
+        alert("Password must contain at a capital character");
         return false;
     }
 	if (x == "^.{1,6}$") {
-        alert("Must contain at least 6 characters");
+        alert("Password must contain at least 6 characters");
         return false;
     }
 	
 	// Mobile Phone
 	var x = document.forms["regform"]["mobphone"].value;
     if (x == "") {
-        alert("Name must be filled out");
+        alert("Phone must be filled out");
         return false;
     }
 	if (x != "\d") {
-        alert("Must be numbers");
+        alert("Phone must be numbers");
         return false;
     }
 	if (x != "^.{8,10}$") {
-        alert("Number is invalid");
+        alert("Phone number is invalid");
         return false;
     }
 	
 	// Email
 	var x = document.forms["regform"]["email"].value;
 	if (x == "") {
-        alert("Name must be filled out");
+        alert("Email must be filled out");
         return false;
     }
 	if (x != "[@]") {
